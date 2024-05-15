e591b1499d7f8675f3e9e854d9923c497180d919
Jin Minu <s9430939@naver.com>
2024-05-16 00:36:25 +0900

    record 액티비티, start액티비티, 버튼 id추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.gymhelper"
-        minSdk = 16
+        minSdk = 19
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
---
+++ app/src/main/java/com/example/gymhelper/StartActivity.java
@@ -0,0 +1,6 @@
+package com.example.gymhelper;
+
+import android.app.Activity;
+
+public class StartActivity extends Activity {
+}
