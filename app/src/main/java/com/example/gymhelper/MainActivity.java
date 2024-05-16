ed7c056f22b902e9e66862f318dbf67b20f3ecab
Jin Minu <s9430939@naver.com>
2024-05-16 10:55:10 +0900

    intent구현

--- app/src/main/java/com/example/gymhelper/MainActivity.java
+++ app/src/main/java/com/example/gymhelper/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.gymhelper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button startBtn;
+    Button recordBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,25 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        startBtn = (Button) findViewById(R.id.startBtn);
+        recordBtn = (Button) findViewById(R.id.recordBtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), StartActivity.class);
+                startActivity(startIntent);
+            }
+        });
+
+        recordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent recordIntent = new Intent(getApplicationContext(), RecordActivity.class);
+                startActivity(recordIntent);
+            }
         });
     }
 }
--- app/src/main/java/com/example/gymhelper/RecordActivity.java
+++ app/src/main/java/com/example/gymhelper/RecordActivity.java
@@ -1,6 +1,16 @@
 package com.example.gymhelper;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 
 public class RecordActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.record_layout);
+
+
+    }
 }
