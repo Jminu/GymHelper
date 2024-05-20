5dc537451c76facedfd3234b41a06a2531054cae
Jin Minu <s9430939@naver.com>
2024-05-20 17:53:03 +0900

    start activity에 오늘 날짜 출력

--- app/src/main/java/com/example/gymhelper/StartActivity.java
+++ app/src/main/java/com/example/gymhelper/StartActivity.java
@@ -2,8 +2,13 @@ package com.example.gymhelper;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class StartActivity extends Activity {
     @Override
@@ -11,6 +16,8 @@ public class StartActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_layout);
 
-
+        String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+        TextView dateTextView = (TextView) findViewById(R.id.dateTextView);
+        dateTextView.setText(currentDate);
     }
 }
