5c6f2bcc1098f7e95c382ae7afce2799b5eaa90c
Jin Minu <s9430939@naver.com>
2024-06-16 01:38:31 +0900

    지도 추가, 지도 버튼 구현

---
+++ app/src/main/java/com/example/gymhelper/MapsActivity.java
@@ -0,0 +1,14 @@
+package com.example.gymhelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MapsActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maps_activity);
+    }
+}
